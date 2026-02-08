6ea538ccc4e8a819909b91d2154acc8403f23b60
seanlucey <146947873+seanlucey@users.noreply.github.com>
2026-02-08 08:56:01 +0000

    Create iam.tf

---
+++ terraform/modules/ecs/iam.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "task_execution" {
+  name = "${var.name}-task-exec"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "task_exec_policy" {
+  role = aws_iam_role.task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
